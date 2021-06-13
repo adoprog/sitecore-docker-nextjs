84ca8f3d1730b0560e7651f5fe8213a94bba1b5a
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-12 21:03:00 -0400

    Added JsonStorage
    Update ContentBlock.tsx
    
    Update tsconfig.json
    
    Updated @jsonstorage

--- src/rendering/src/components/ContentBlock.tsx
+++ src/rendering/src/components/ContentBlock.tsx
@@ -1,5 +1,11 @@
 import { Text, RichText, Field } from '@sitecore-jss/sitecore-jss-nextjs';
 import { StyleguideComponentProps } from 'lib/component-props';
+import { usePersonalization } from "@jsonstorage/personalize-react";
+import { ExperimentConfig } from '@jsonstorage/personalize-react/lib/interfaces';
+
+const config = {
+  id: "fa0f4907-6c9a-4ba4-82ac-3658ff5ed760"
+} as ExperimentConfig;
 
 type ContentBlockProps = StyleguideComponentProps & {
   fields: {
@@ -13,12 +19,16 @@ type ContentBlockProps = StyleguideComponentProps & {
  * This is the most basic building block of a content site, and the most basic
  * JSS component that's useful.
  */
-const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => (
-  <>
-    <Text tag="h2" className="display-4" field={fields.heading} />
+const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => {
+  const variant = usePersonalization(config);
+
+  return(
+  <div className={variant?.data.style}>
+    <Text tag="h2" className="display-4" field={fields.heading}  />
+    <img src={variant?.data.image} width="500px"></img>
 
     <RichText className="contentDescription" field={fields.content} />
-  </>
-);
+  </div>)
+};
 
 export default ContentBlock;
